73505fe8b3eac99cf07cf000af55948d8cdd8e14
Daniel Satnic <S01031109@acad.tri-c.edu>
2022-05-07 16:41:44 -0400

    got the push to work

--- src/DSatnicStack.java
+++ src/DSatnicStack.java
@@ -1,3 +1,42 @@
 public class DSatnicStack {
+	private int[] myStack;
+	
+	public void SeeStack() {
+		System.out.println("................");
+		if (this.myStack == null) {
+			System.out.println("Stack is empty!");
+		} else {
+			System.out.println("Current Amounts:");
+			for (int i = 0; i < this.myStack.length; i++) {
+				System.out.println("Position: " + i + " ... " + "Value: " + this.myStack[i]);
+			}
+		}
+		System.out.println("................");
+	}
+	
+	public void Push(int value) {
+		if (this.myStack == null || this.myStack.length == 0) {
+			this.myStack = new int[1];
+			this.myStack[0] = value;
+			return;
+		}
 
+		this.PushLogic(value, this.myStack.length);
+	}
+	
+	private void PushLogic(int value, int position) {
+		var newArray = new int[this.myStack.length + 1];
+
+		for (int i = 0; i < position; i++) {
+			newArray[i] = this.myStack[i];
+		}
+
+		newArray[position] = value;
+
+		for (int i = position + 1; i <= this.myStack.length; i++) {
+			newArray[i] = this.myStack[i - 1];
+		}
+
+		this.myStack = newArray;
+	}
 }
--- src/StackRunner.java
+++ src/StackRunner.java
@@ -1,5 +1,14 @@
 public class StackRunner {
+	static DSatnicStack dSatnicStack = new DSatnicStack();
+	
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		dSatnicStack.SeeStack();
+		
+		var valuesToPush = new int[] { 4, 67, 73, 53, 57, 12 };
+		for (int i = 0; i < valuesToPush.length; i++) {
+			dSatnicStack.Push(valuesToPush[i]);
+		}
+		
+		dSatnicStack.SeeStack();
 	}
 }
